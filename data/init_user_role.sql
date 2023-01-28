2be109c0fc010aac692f024802aee43fc66dff76
dennisbrian <dennis-looi@hotmail.com>
2023-01-28 23:22:03 +0800

    1) Update the init data for guide

--- data/init_user_role.sql
+++ data/init_user_role.sql
@@ -1,4 +1,4 @@
 INSERT INTO `user_role`
 (`name`,`label`,`description`,`created_at`,`updated_at`,`is_delete`) VALUES
-('admin','Admin','Administrator','2020-04-17 10:42:00',null,0),
-('subadmin','Sub Admin','Sub Admin','2020-04-17 10:42:00',null,0);
+('admin','Admin','Administrator',null,null,0),
+('user','User','User',null,null,0);
